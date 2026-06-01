5a87be88d95235028a387a6fd9e0b79e44c4091d
Ieva Jonusyte <75742395+ievajonusyte@users.noreply.github.com>
2026-06-01 12:57:54 +0200

    Implement integer power calculation

---
+++ Math/integers_come_in_all_sizes.py
@@ -0,0 +1,12 @@
+'''
+Task: Read 4 integers (a, b, c, d) and print the result of a^b + c^d
+Input: 4 numbers, each on a separate line
+Constraints: All values between 1 and 1000
+Output: Single line with the result
+'''
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(a**b + c**d)
