6c8bc1da3c1764581b53d75c78ccfc7135c237e0
Ieva Jonusyte <75742395+ievajonusyte@users.noreply.github.com>
2026-06-18 10:56:52 +0200

    Clarify docstring and output instructions
    Updated docstring for clarity and added instructions for output.

--- Regex_and_parsing/validating_phone_numbers.py
+++ Regex_and_parsing/validating_phone_numbers.py
@@ -1,6 +1,7 @@
 """
-Validating Phone Numbers
-A valid mobile number is a ten digit number starting with 7, 8 or 9.
+Validating Phone Numbers:
+a valid mobile number is a ten digit number starting with 7, 8 or 9.
+For every string listed, print "YES" if it is a valid mobile number and "NO" if it is not on separate lines. Do not print the quotes.
 """
 
 import re
@@ -10,7 +11,8 @@ n = int(input()) # reads how many numbers will follow
 for _ in range(n): # loops exactly input times
     number = input() # reads one number per loop iteration
     if re.match(r'^[789]\d{9}$', number):
-        #^start of string,
+        #r means raw prefix,take this string exactly as typed, no special treatment for backslashes
+        #^start of string
         #[789]first digit must be 7, 8, or 9
         #/d{9}exactly 9 more digits after that
         #$end of string
