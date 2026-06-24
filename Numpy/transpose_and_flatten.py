3364723b7261fed7a20d5e763685423f62cbdc6f
Ieva Jonusyte <75742395+ievajonusyte@users.noreply.github.com>
2026-06-24 15:59:34 +0200

    Add docstring for transpose and flatten script
    Added a docstring explaining the function of the script.

--- Numpy/transpose_and_flatten.py
+++ Numpy/transpose_and_flatten.py
@@ -1,5 +1,8 @@
 import numpy
-
+'''
+You are given a N X Minteger array matrix with space separated elements (N = rows and  M = columns).
+Your task is to print the transpose and flatten results.
+'''
 n, m = map(int, input().split()) # reads the first line and gets the dimensions
 arr = numpy.array([input().split() for _ in range(n)], int) # reads n more lines, each split into a list of strings
 
